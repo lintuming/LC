1fddf48865b37c42f7a2b0168ad0cadaea0ac6a5
lintuming <lintuming@tuzhanai.com>
2019-11-11 17:39:44 +0800

    interleave

--- 97.interleaving-string.js
+++ 97.interleaving-string.js
@@ -12,7 +12,24 @@
  * @return {boolean}
  */
 var isInterleave = function(s1, s2, s3) {
-    
+  const optimized={}
+  function is(...args) {
+    const [s1, s2, s3] = args;
+    if (args.every(s => s.length === 0)) {
+      return true;
+    }
+    if (s1.length + s2.length !== s3.length) {
+      return false;
+    }
+    const first = s3[0];
+    if (first === s1[0] && is(s1.slice(1), s2, s3.slice(1))) {
+      return true;
+    } else if (first === s2[0] && is(s1, s2.slice(1), s3.slice(1))) {
+      return true;
+    }
+    return false;
+  }
+  return is(s1, s2, s3);
 };
 // @lc code=end
-
+console.log(isInterleave('aabcc','dbbca','aadbbbaccc'))
